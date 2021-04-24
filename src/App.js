cc0796f0e3ba15dbebe1ef427891e433275ad362
gabrielfresco94 <83130964+gabrielfresco94@users.noreply.github.com>
2021-04-24 20:57:05 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Pokedex from "./components/Pokedex";
 import SearchBar from "./components/SearchBar";
 import "./styles.css";
+import { getPokemons } from "../api";
 
 export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  const fecthPokemons = async () => {
+    try {
+      const data = await getPokemons();
+      setPokemons(data.results);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fecthPokemons();
+  }, []);
   return (
     <>
       <Navbar />
       <div className="App">
         <SearchBar />
       </div>
-      <Pokedex />
+      <Pokedex pokemons={pokemons} />
     </>
   );
 }
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -15,7 +15,7 @@ const PokedexGrid = styled.div`
   grid-gap: 10px;
 `;
 
-const Pokedex = () => {
+const Pokedex = ({ pokemons }) => {
   return (
     <div>
       <HeaderPokedex>
@@ -23,16 +23,9 @@ const Pokedex = () => {
         <div>Pagination</div>
       </HeaderPokedex>
       <PokedexGrid>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-        <div>6</div>
-        <div>7</div>
-        <div>8</div>
-        <div>9</div>
-        <div>10</div>
+        {pokemons.map((pokemon, index) => {
+          return <div>{pokemon.name}</div>;
+        })}
       </PokedexGrid>
     </div>
   );
